a28e5219c699afee9e8ae2c67b2d3404320b47b8
Matrix Mash <matrixmash10@gmail.com>
2021-06-25 14:39:48 -0400

    Clean up

--- client.py
+++ client.py
@@ -10,17 +10,17 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 personal_path = os.path.join(script_dir, 'personal.txt')
 
 with open(personal_path) as personal_file:
-    api_key = next(iter(personal_file)).strip()
+    api_key = personal_file.read().splitlines()[0].strip()
 
 URL_BASE = 'https://e621.net/'
 USER_AGENT = "ConMan/1.2 (e621 tagging interface by MatrixMash)"
 HEADERS_BASE = {'user-agent':USER_AGENT}
 
 def get(url, **kwargs):
-    time.sleep(1)
+    time.sleep(1) # Rate limiting lol
     return requests.get(url, **kwargs, headers=HEADERS_BASE)
 
-class Search:
+class LazySearch:
     def __init__(self, search_string):
         self.search_string = search_string
         self.cache_limit = 6
@@ -33,34 +33,20 @@ class Search:
             params['page'] = 'b' + str(self.before_id)
         self.cache = get(URL_BASE + 'posts.json', params=params).json()
     def posts(self):
-        for _ in range(3):
-            #r = get(URL_BASE + 'posts.json?limit=10')
-            #jso = r.json()
-            #jso = json.load(open('toy.txt'))
-            yield from self.cache['posts'] #jso['posts']
+        while True:
+            yield from self.cache['posts']
             if len(self.cache['posts']) != self.cache_limit:
                 break
             self.before_id = self.cache['posts'][-1]['id']
             self.load()
-    
 
 def run():
-    #json.dump(jso, open('toy.txt', 'w'))
-    search = Search('duo')
+    search = LazySearch('duo')
     
     for p in search.posts():
         file_metadata = p['file']
         file_url = file_metadata['url']
-        file_width = file_metadata['width']
-        file_height = file_metadata['height']
         print(file_url)
-        #print(p['sample']['url'])
-        #print(p['preview']['url'])
-    
-    
-    #from pprint import pprint
-    #pprint(jso)
-    
 
 if __name__ == '__main__':
     run()
