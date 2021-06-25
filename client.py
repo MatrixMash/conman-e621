b50c63747ba40cd0b88a052cf81eafcea9aede62
Matrix Mash <matrixmash10@gmail.com>
2021-06-25 14:08:54 -0400

    Find the image location stuff.

--- client.py
+++ client.py
@@ -12,10 +12,29 @@ URL_BASE = 'https://e621.net/'
 USER_AGENT = "ConMan/1.2 (e621 tagging interface by MatrixMash)"
 HEADERS_BASE = {'user-agent':USER_AGENT}
 
+#class RequestsLimiter
+
 def run():
-    r = requests.get(URL_BASE + 'posts.json?limit=1', headers=HEADERS_BASE)
+    r = requests.get(URL_BASE + 'posts.json?limit=10', headers=HEADERS_BASE)
     jso = r.json()
-    json.dump(jso, sys.stdout)
+    #jso = json.load(open('toy.txt'))
+    #json.dump(jso, open('toy.txt', 'w'))
+    
+    post_listing = jso
+    posts = post_listing['posts']
+    for p in posts:
+        file_metadata = p['file']
+        file_url = file_metadata['url']
+        file_width = file_metadata['width']
+        file_height = file_metadata['height']
+        print(file_url)
+        #print(p['sample']['url'])
+        #print(p['preview']['url'])
+    
+    
+    #from pprint import pprint
+    #pprint(jso)
+    
 
 if __name__ == '__main__':
     run()
