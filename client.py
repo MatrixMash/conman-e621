2415b315b31d8fecbdab723c1d3092610f293e9d
Matrix Mash <matrixmash10@gmail.com>
2021-06-27 16:05:48 -0400

    Move rate limiting into its own class.
    This will eventually implement caching and preloading.

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
 import os, sys
 import json
-import requests
 import time
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
+import requests
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 personal_path = os.path.join(script_dir, 'personal.txt')
 
+username = 'matrixmash10'
 with open(personal_path) as personal_file:
     api_key = personal_file.read().splitlines()[0].strip()
 
@@ -16,9 +15,12 @@ URL_BASE = 'https://e621.net/'
 USER_AGENT = "ConMan/1.2 (e621 tagging interface by MatrixMash)"
 HEADERS_BASE = {'user-agent':USER_AGENT}
 
-def get(url, **kwargs):
-    time.sleep(1) # Rate limiting lol
-    return requests.get(url, **kwargs, headers=HEADERS_BASE)
+class ResourceManager:
+    def get_url(self, url, **kwargs):
+        time.sleep(1) # Rate limiting lol
+        return requests.get(url, **kwargs, headers=HEADERS_BASE)
+
+resource_manager = ResourceManager()
 
 class LazySearch:
     def __init__(self, search_string):
@@ -31,9 +33,10 @@ class LazySearch:
         params = {'limit':self.cache_limit, 'tags':self.search_string}
         if self.before_id is not None:
             params['page'] = 'b' + str(self.before_id)
-        self.cache = get(URL_BASE + 'posts.json', params=params).json()
+        self.cache = resource_manager.get_url(URL_BASE + 'posts.json', params=params).json()
     def posts(self):
-        while True:
+        for _ in range(3):
+        #while True:
             yield from self.cache['posts']
             if len(self.cache['posts']) != self.cache_limit:
                 break
